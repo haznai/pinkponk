8f4bae3036ee9838d1f1261f45486b6a4cdcf4fe
hazn <aitakprojects@gmail.com>
2025-02-01 15:57:51 +0100

    pipo-0: add tests and `async-http-client` dependency

---
+++ tests/tests.swift
@@ -0,0 +1,12 @@
+// todo: remove this header comment
+
+import Testing
+
+struct tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        #expect(false)
+    }
+
+}
