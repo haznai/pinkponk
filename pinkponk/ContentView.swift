9e0853b5c55efe83ee4cf2e3678dc0361ec2f762
hazn <aitakprojects@gmail.com>
2025-02-10 07:31:54 +0100

    pipo-0: apply formatting

--- pinkponk/ContentView.swift
+++ pinkponk/ContentView.swift
@@ -1,34 +1,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    // todo: refactor/make it more genral
-    @State private var readwiseState: [Readwise] = []
-    @State private var apiKey: String = ""
+  // todo: refactor/make it more genral
+  @State private var readwiseState: [Readwise] = []
+  @State private var apiKey: String = ""
   var body: some View {
-      VStack {
-          SecureField("Readwise API Key", text: $apiKey)
-          Button("let's get this bread") {
-              Task {
-                  // todo: make anki card that
-                  // task without `do` `catch` silently swallows errors
-                  do {
-                         let state = try await ReadwiseRepository(apiKey: apiKey).fetchData()
-                         await MainActor.run {
-                             self.readwiseState = state
-                         }
-                     } catch {
-                         print("Error: \(error)")
-                        //todo: better error handling
-                     }
-              }
+    VStack {
+      SecureField("Readwise API Key", text: $apiKey)
+      Button("let's get this bread") {
+        Task {
+          // todo: make anki card that
+          // task without `do` `catch` silently swallows errors
+          do {
+            let state = try await ReadwiseRepository(apiKey: apiKey).fetchData()
+            await MainActor.run {
+              self.readwiseState = state
+            }
+          } catch {
+            print("Error: \(error)")
+            //todo: better error handling
           }
-          
-              
-          List(readwiseState) { item in
-                        Text(item.title ?? "No title")
-                    }
+        }
+      }
+
+      List(readwiseState) { item in
+        Text(item.title ?? "No title")
       }
     }
+  }
 }
 
 #Preview {
