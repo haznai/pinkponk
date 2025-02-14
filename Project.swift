9b9052bbae95634864f20f8f7311e80f78531514
hazn <aitakprojects@gmail.com>
2025-02-14 16:11:47 +0100

    pipo-0: add `inlineSnapshotTesting` dependency
    also, `swift format` hehehe

--- Project.swift
+++ Project.swift
@@ -1,30 +1,36 @@
 import ProjectDescription
 
 let project = Project(
-    name: "pinkponk",
-    packages: [.remote(url: "https://github.com/Lighter-swift/Lighter", requirement: .upToNextMajor(from: "1.4.10"))],
-    targets: [
-        .target(
-            name: "pinkponk",
-            destinations: .macOS,
-            product: .app,
-            bundleId: "io.tuist.pinkponk",
-            deploymentTargets: .macOS("15.1"),
-            infoPlist: .default,
-            sources: ["pinkponk/Sources/**"],
-            resources: ["pinkponk/Resources/**"],
-            dependencies: [.external(name: "AsyncHTTPClient"), .package(product: "Enlighter", type: .plugin), .external(name: "Lighter")]
-            ),
-        .target(
-            name: "pinkponkTests",
-            destinations: .macOS,
-            product: .unitTests,
-            bundleId: "io.tuist.pinkponkTests",
-            deploymentTargets: .macOS("15.1"),
-            infoPlist: .default,
-            sources: ["pinkponk/Tests/**"],
-            resources: [],
-            dependencies: [.target(name: "pinkponk")]
-        ),
-    ]
+  name: "pinkponk",
+  packages: [
+    .remote(
+      url: "https://github.com/Lighter-swift/Lighter", requirement: .upToNextMajor(from: "1.4.10"))
+  ],
+  targets: [
+    .target(
+      name: "pinkponk",
+      destinations: .macOS,
+      product: .app,
+      bundleId: "io.tuist.pinkponk",
+      deploymentTargets: .macOS("15.1"),
+      infoPlist: .default,
+      sources: ["pinkponk/Sources/**"],
+      resources: ["pinkponk/Resources/**"],
+      dependencies: [
+        .external(name: "AsyncHTTPClient"), .package(product: "Enlighter", type: .plugin),
+        .external(name: "Lighter"), .external(name: "InlineSnapshotTesting"),
+      ]
+    ),
+    .target(
+      name: "pinkponkTests",
+      destinations: .macOS,
+      product: .unitTests,
+      bundleId: "io.tuist.pinkponkTests",
+      deploymentTargets: .macOS("15.1"),
+      infoPlist: .default,
+      sources: ["pinkponk/Tests/**"],
+      resources: [],
+      dependencies: [.target(name: "pinkponk")]
+    ),
+  ]
 )
--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -2,23 +2,24 @@
 import PackageDescription
 
 #if TUIST
-    import struct ProjectDescription.PackageSettings
+  import struct ProjectDescription.PackageSettings
 
-    let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,]
+  let packageSettings =
+    PackageSettings(// Customize the product types for specific package product
+    // Default is .staticFramework
+    // productTypes: ["Alamofire": .framework,]
     )
 #endif
 
 let package = Package(
-    name: "pinkponk",
-    dependencies: [
-      .package(url: "https://github.com/swift-server/async-http-client", from: "1.25.1"),
-      // Careful: This package and version has been defined in `Project.Swift` aswell
-      // The `Project.Swift` containts the Build Tool plugin, here we import the `Lighter` Library used
-      // By the derivatives of the Enlighter Build tool Plugin.
-      .package(url: "https://github.com/Lighter-swift/Lighter", from: "1.4.10")
-    ],
-    swiftLanguageModes: [.v6]
+  name: "pinkponk",
+  dependencies: [
+    .package(url: "https://github.com/swift-server/async-http-client", from: "1.25.1"),
+    // Careful: This package and version has been defined in `Project.Swift` aswell
+    // The `Project.Swift` containts the Build Tool plugin, here we import the `Lighter` Library used
+    // By the derivatives of the Enlighter Build tool Plugin.
+    .package(url: "https://github.com/Lighter-swift/Lighter", from: "1.4.10"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.0"),
+  ],
+  swiftLanguageModes: [.v6]
 )
