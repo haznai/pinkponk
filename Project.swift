75c1763a8d7d670fa107952bf9a411ccdad86d4e
hazn <aitakprojects@gmail.com>
2025-02-15 12:55:30 +0100

    pipo-0: enable swift 6 language mode

--- Project.swift
+++ Project.swift
@@ -1,9 +1,20 @@
 import ProjectDescription
 
+func baseSettings() -> SettingsDictionary {
+  var settings: SettingsDictionary = [:]
+  settings["SWIFT_VERSION"] = "6"
+  settings["_EXPERIMENTAL_SWIFT_EXPLICIT_MODULES"] = "true"
+  settings["SWIFT_STRICT_CONCURRENCY"] = "complete"
+
+  return settings
+}
+
 let project = Project(
   name: "pinkponk",
   packages: [
     .remote(
+      // This is declaration for the "Enlighter" Plugin, the `Package.swift` file has the
+      // declaration for the "Lighter" Dependency
       url: "https://github.com/Lighter-swift/Lighter", requirement: .upToNextMajor(from: "1.4.10"))
   ],
   targets: [
@@ -17,9 +28,13 @@ let project = Project(
       sources: ["pinkponk/Sources/**"],
       resources: ["pinkponk/Resources/**"],
       dependencies: [
-        .external(name: "AsyncHTTPClient"), .package(product: "Enlighter", type: .plugin),
-        .external(name: "Lighter"), .external(name: "InlineSnapshotTesting"),
-      ]
+        .external(name: "AsyncHTTPClient"),
+        .package(product: "Enlighter", type: .plugin),
+        .external(name: "Lighter"),
+        .external(name: "InlineSnapshotTesting"),
+      ],
+      settings: Settings.settings(
+        base: baseSettings())
     ),
     .target(
       name: "pinkponkTests",
@@ -30,7 +45,9 @@ let project = Project(
       infoPlist: .default,
       sources: ["pinkponk/Tests/**"],
       resources: [],
-      dependencies: [.target(name: "pinkponk")]
+      dependencies: [.target(name: "pinkponk")],
+      settings: Settings.settings(
+        base: baseSettings())
     ),
   ]
 )
