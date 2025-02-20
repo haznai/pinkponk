f18dd2fc5548e658c3477a546e010480a6c9bd3b
hazn <aitakprojects@gmail.com>
2025-02-20 18:50:26 +0100

    pipo-9: add tests for `ReadwiseRepository.swift`

--- pinkponk/Tests/tests.swift
+++ pinkponk/Tests/tests.swift
@@ -6,11 +6,17 @@ import Testing
 
 struct AsyncHTTPClientTests {
   @MainActor @Test func readwiseRepo() throws {
-    let repo = ReadwiseRepository()
-    // api key passed
-    assertInlineSnapshot(of: repo.getRequest(with: "test-api-key"), as: .description) {
+    let repo = ReadwiseRepository(apiKey: "test-api-key")
+    assertInlineSnapshot(of: repo.getRequest(), as: .description) {
       """
       HTTPClientRequest(url: "https://readwise.io/api/v3/list/", method: NIOHTTP1.HTTPMethod.GET, headers: Authorization: Token test-api-key, body: nil, tlsConfiguration: nil)
+      """
+    }
+    assertInlineSnapshot(
+      of: repo.getRequest(lastPaginationCursor: "test-last-pagination-cursor"), as: .description
+    ) {
+      """
+      HTTPClientRequest(url: "https://readwise.io/api/v3/list/?pageCursor=test-last-pagination-cursor", method: NIOHTTP1.HTTPMethod.GET, headers: Authorization: Token test-api-key, body: nil, tlsConfiguration: nil)
       """
     }
   }
